847e7f1da3efd42e5f614cde4f4a65bac34c4fd6
pritika <pritikasabharwal@gmail.com>
2020-04-15 15:08:32 +0000

    add mongodb atlas

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ var commentroutes=require("./routes/comments");
 var campgroundroutes=require("./routes/campgrounds");
 var indexroutes=require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser: true});
+//mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser: true});
+mongoose.connect("mongodb+srv://pritika:$sonia77@cluster0-ongjw.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser: true});
 app.use(bodyparser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));
